ee0be966423032ee555f4c079e3e9ab21a5d7c21
David Gyenes <davee@fxp.hu>
2020-07-28 16:02:05 +0200

    make group is optional

--- src/server/modules/digionline/libs/generate-playlist.ts
+++ src/server/modules/digionline/libs/generate-playlist.ts
@@ -2,6 +2,7 @@ import { slug } from '../../../lib';
 import { Channel } from '../../../interfaces';
 import extraChannels from '../../../../../config/extra_channels.json';
 import { sortBy, compose, toLower, prop, assoc } from 'ramda';
+import config from '../../../../../config/config.json';
 
 const sortByNameCaseInsensitive = sortBy(compose(toLower, prop('name')));
 
@@ -11,10 +12,11 @@ export function generatePlaylist(channels: Channel[], deviceId: number, quality:
     `#EXTM3U tvg-shift="${(new Date().getTimezoneOffset() / 60) * -1}"\n` +
     mergedChannels
       .map((channel) => {
+        const group = config.groups ? ` group-title="${channel.category.name}"` : '';
         const url = channel['extra'] ? channel.url : `${channel.url}/${deviceId}/${quality}/stream.m3u8`;
         return `#EXTINF:${channel.id} tvg-id="${slug(channel.name)}" tvg-name="${channel.name}" tvg-logo="${
           channel.logoUrl
-        }" group-title="${channel.category.name}", ${channel.name}\n${url}`;
+        }"${group}, ${channel.name}\n${url}`;
       })
       .join('\n')
   );
